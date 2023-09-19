0b23dacb1b1c16a04cfe1ccdb48ebf4b5d99d81b
Paul Panosch <paulpanosch@gmx.at>
2023-09-19 22:11:34 +0200

    Das Auslesen der Http Request über einen TCP Socket geht endlich

--- MonsterCardGame/Models/User.cs
+++ MonsterCardGame/Models/User.cs
@@ -11,12 +11,18 @@ namespace MonsterCardGame.Models
         public string Username { get; private set; }
         public CardStack CardStack { get; private set; }
         public int Coins { get; private set; }
+        public int Elo { get; private set; }
+        public int Wins { get; private set; }
+        public int Losses { get; private set; }
 
-        public User(string username, CardStack cardStack, int coins) 
+        public User(string username, CardStack cardStack, int coins, int elo, int wins, int losses) 
         {
             Username = username;
             CardStack = cardStack;
             Coins = coins;
+            Elo = elo;
+            Wins = wins;
+            Losses = losses;
         }
     }
 }
